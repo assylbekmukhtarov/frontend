2a45aa195024a66fc04b5f8a9d5345965fd5bbc4
Assylbek Mukhtarov <astat730@gmail.com>
2026-02-17 01:49:27 +0500

    test 1 lesson3 with variables

--- agpc/src/App.vue
+++ agpc/src/App.vue
@@ -1,14 +1,38 @@
 <script >
-
+export default {
+  data() {
+    return {
+      info: 'Hello World',
+      someInfo: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, doloremque.",
+      some:{
+        name: "John Doe",
+        age: 30,
+        city: "New York"
+      }
+    }
+  }
+}
 </script>
 
 <template>
- <h1>AGPC</h1>
+ <h3>{{ info }}</h3>
+  <p>{{ someInfo }}</p>
+  <p class="info">Name: {{ some.name }}</p>
+  <p class="info">Age: {{ some.age }}</p>
+  <p class="info">City: {{ some.city }}</p>
+
 
 </template>
 
 <style scoped>
-h1 {
- color: RED;
+h3 {
+ font-weight: lighter;
+
+}
+p{
+  color: red;
+}
+.info{
+  color: blue;
 }
 </style>
