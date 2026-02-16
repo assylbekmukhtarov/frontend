7d2fa0c05df5fa467724ef1bdd938064acc53ecc
Assylbek Mukhtarov <astat730@gmail.com>
2026-02-17 02:05:17 +0500

    пользователи, обработка ошибок, проверка на пустые поля

--- agpc/src/App.vue
+++ agpc/src/App.vue
@@ -2,15 +2,28 @@
 export default {
   data() {
     return {
-      info: 'Hello World',
-      someInfo: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, doloremque."
+      users:[],
+      error: '',
+      userName: 'John Doe',
+      userPass: '',
+      userEmail: ''
+
   }
 }
 ,
   methods: {
-    userData(word = "John Doe") {
-      this.someInfo = "User data updated!"
-      this.info = "User Name: " + word
+    sendData() {
+      if(this.userName=='' || this.userPass==='' || this.userEmail===''){
+        this.error = 'PLEASE FILL ALL FIELDS';
+        return;
+      }
+      this.error = '';
+
+      this.users.push({
+        name: this.userName,
+        email: this.userEmail,
+        password: this.userPass
+      });
     }
     
   }
@@ -20,20 +33,54 @@ export default {
 </script>
 
 <template>
- <h3>{{ info }}</h3>
-  <p>{{ someInfo }}</p>
-  <button @click="userData()" @mouseenter="info = 'Mouse Hovered!'">Change Info</button>
+  <input type="text" v-model="userName" placeholder="Enter your name">
+  <input type="password" v-model="userPass" placeholder="Enter your password">
+  <input type="email" v-model="userEmail" placeholder="Enter your email">
+  <p class="error">{{ error }}</p>
+  <button @click="sendData()">Submit</button>
+  <p>{{ users }}</p>
 </template>
 
 <style scoped>
-h3 {
- font-weight: lighter;
+input{
+  width:100%;
+  padding:10px 12px;
+  margin:8px 0;
+  border:1px solid #ccc;
+  border-radius:8px;
+  outline:none;
+}
 
+input:focus{
+  border-color:#4f46e5;
+  box-shadow:0 0 0 3px rgba(79,70,229,.2);
 }
+
+button{
+  width:100%;
+  padding:10px 12px;
+  margin-top:10px;
+  border:none;
+  border-radius:8px;
+  background:#4f46e5;
+  color:#fff;
+  cursor:pointer;
+}
+
+button:hover{
+  opacity:.9;
+}
+
 p{
-  color: red;
+  margin-top:12px;
+  padding:10px 12px;
+  border:1px dashed #ccc;
+  border-radius:8px;
+  color:#333;
+  word-break:break-word;
 }
-.info{
-  color: blue;
+.error{
+  color:red;
+  font-weight:bold;
 }
 </style>
